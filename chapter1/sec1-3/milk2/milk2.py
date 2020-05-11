bb62f2202619489372e39465521b3a1b5ba4886a
Kevin Vicente <kvicente@umass.edu>
2020-05-10 23:45:15 -0400

    finished milk, namenum and palsquare

--- chapter1/sec1-3/milk2/milk2.py
+++ chapter1/sec1-3/milk2/milk2.py
@@ -11,12 +11,62 @@ TASK: milk2
 302 402
 405 503
 """
+import functools
 
 n = 0
-farmers = []
+intervals = []
 with open("milk2.in", 'r') as fio:
   n = int(fio.readline())
   for i in range(0, n):
-    farmers.append(fio.readline().split(' '))
-    farmers[i] = (int(farmers[i][0]), int(farmers[i][1]))
+    intervals.append(fio.readline().split(' '))
+    intervals[i] = (int(intervals[i][0]), int(intervals[i][1]))
 
+def length(interval):
+  return interval[1] - interval[0]
+
+def intervals_interlap(i1, i2):
+  return i2[0] <= i1[1] and i2[0] >= i1[0] or i1[0] <= i2[1] and i1[0] >= i2[0]
+
+# assumes intervals is nonempty
+# will find the longest continuous intervals starting at the first farmers starting time.
+# each interval is seperated by some amount of time -- i.e. none of the intervals overlap.
+def generate_longest_intervals(intervals):
+  longest_intervals = []
+  current_longest = intervals[0]
+  for i in range(1, len(intervals)):
+    if not intervals_interlap(intervals[i], current_longest):
+      longest_intervals.append(current_longest)
+      current_longest = intervals[i]
+    elif intervals_interlap(intervals[i], current_longest):
+      current_longest = (current_longest[0], max(current_longest[1], intervals[i][1]))
+
+  longest_intervals.append(current_longest)
+  return longest_intervals
+
+
+# sort the intervals by the start time for each farmer in ascending order
+intervals.sort(key=lambda a: a[0])
+longest_milking_intervals = generate_longest_intervals(intervals)
+longest_non_milking_intervals = [
+(longest_milking_intervals[i][1], longest_milking_intervals[i+1][0])
+for i in range(0, len(longest_milking_intervals) - 1)
+]
+
+max_milking_interval_length = functools.reduce(
+  lambda a,b: a if a>b else b, map(length, longest_milking_intervals))
+
+if n == 1:
+  max_non_milking_interval_length = 0
+else:
+  max_non_milking_interval_length = functools.reduce(
+    lambda a,b: a if a>b else b, map(length, longest_non_milking_intervals), 0)
+
+"""
+print(intervals)
+print(sorted)
+print(longest_milking_intervals)
+print(max_milking_interval_length)
+print(max_non_milking_interval_length)
+"""
+with open("milk2.out", "w") as fio:
+  fio.write(str(max_milking_interval_length) + " " + str(max_non_milking_interval_length) + "\n")
--- chapter1/sec1-3/palsquare/palsquare.py
+++ chapter1/sec1-3/palsquare/palsquare.py
@@ -4,15 +4,53 @@ LANG: PYTHON3
 TASK: palsquare
 """
 
+from math import log, floor
+
 base = 0
+palindromic_squares = []
 with open("palsquare.in", 'r') as fio:
   base = int(fio.readline())
 
-def log_base(n, b):
-  return log(n) / log(b)
+def is_palindrome(string):
+  if len(string) <= 1:
+    return True
+  if len(string) == 2:
+    return string[0] == string[1]
 
-def num_digits_in_base(n, b):
-  return (n % b == 0) ? log_base(n, b) : log_base(n, b) + 1
+  mid = len(string) // 2
+  low = mid - 1
+  high = mid + 1 if len(string) % 2 != 0 else mid
+  while low >= 0 and high < len(string):
+    if string[low] != string[high]:
+      return False
+    low -= 1
+    high += 1
+  return True
 
-def is_palindromic_in_base(n, b):
-  length = num_digits(n)
+# takes two naturals, returns a string
+def to_base(num, base):
+  # The exponent of the base corresponding to the most significant digit in num
+  # e.g. base = 2, num = 3 = 11 in base 2. The most significant digit is in place floor(log_2(3))
+  # = floor(1.58) = 1, 2^1 = 2 and the 2's place has the most significant digit
+  base_exponent = floor(log(num, base))
+  converted = ""
+  while base_exponent >= 0:
+    place_unit = int(base ** base_exponent)
+    multiplier = int((num - (num % place_unit)) / place_unit)
+    if multiplier >= 10:
+      converted += chr(ord("A") - 10 + multiplier)
+    else:
+      converted += str(multiplier)
+    num %= place_unit
+    base_exponent -= 1
+  return converted
+
+
+for k in range(1, 301):
+  squared = to_base(k * k, base)
+  if is_palindrome(squared):
+    palindromic_squares.append((to_base(k, base), squared))
+
+with open("palsquare.out", "w") as fio:
+  for (k, square) in palindromic_squares:
+    fio.write(k + " " + square + "\n")
